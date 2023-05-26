ff657df0a5e09398b854276c73a4429034cba409
Dusty Brigsby <108590519+dustybrigsby@users.noreply.github.com>
2023-05-25 18:31:15 -0700

    Added event listeners to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,18 @@ const startPage = document.getElementById("start-page"),
   endPage = document.getElementById("end-page"),
   score = document.getElementById("score"),
   initials = document.getElementById("initials"),
-  submitBtn = document.getElementById("submit-button");
+  submitBtn = document.getElementById("submit-button"),
+  highScores = [];
 let secondsLeft = 75,
-  highScores = localStorage.getItem(highScores);
+  questionNum = 0;
+
+//highScores = localStorage.getItem(highScores);
 
 // Make questions and answers into objects
 const q1 = {
     question: "Commonly used data types DO NOT include:",
     options: ["strings", "booleans", "alerts", "numbers"],
-    answer: 2,
+    answer: "alerts",
   },
   q2 = {
     question:
@@ -27,7 +30,7 @@ const q1 = {
       "parentheses",
       "brackets (square/box brackets)",
     ],
-    answer: 2,
+    answer: "parentheses",
   },
   q3 = {
     question: "Arrays in JavaScript can be used to store _____.",
@@ -37,7 +40,7 @@ const q1 = {
       "booleans",
       "all of the above",
     ],
-    answer: 3,
+    answer: "all of the above",
   },
   q4 = {
     question:
@@ -48,45 +51,78 @@ const q1 = {
       "quotes",
       "parentheses",
     ],
-    answer: 2,
+    answer: "quotes",
   },
   q5 = {
     question:
       "A very useful tool used during development and debugging for printing content to the debugger is:",
     options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-    answer: 3,
+    answer: "console.log",
   },
   // Put Q&A objects into array
   questions = [q1, q2, q3, q4, q5];
 
-//
-function takeQuiz() {
-  // Hides startPage, shows questions
-  startPage.className("hidden");
-  questions.classList.remove("hidden");
-
-  // Starts the quiz timer
-  setTime();
-}
-
-function gameOver() {
-  // Hides questions, shows endPage
-  questions.className("hidden");
-  endPage.classList.remove("hidden");
-}
-
 // Countdown timer, runs at 1 sec intervals starting at 75 secs
 function setTime() {
+  startQuiz();
   let timerInterval = setInterval(function () {
     secondsLeft--;
     timer.textContent = secondsLeft;
 
     if (secondsLeft === 0) {
       clearInterval(timerInterval);
+      score.textContent = secondsLeft;
       gameOver();
     }
   }, 1000);
 }
 
+function startQuiz() {
+  if (questionNum === 0) {
+    // Hides startPage, shows questions
+    startPage.classList.add("hidden");
+    quizzer.classList.toggle("hidden");
+    setQuestion();
+  } else if (questionNum > 4) {
+    clearInterval(timerInterval);
+    score.textContent = secondsLeft;
+    gameOver();
+  }
+}
+
+// Iterates over questions array
+function setQuestion() {
+  let choices = questions[questionNum].options;
+
+  questions[questionNum].question;
+
+  for (let i = 0; i < choices.length; i++) {
+    let btn = document.createElement("button");
+
+    btn.innerText(choices[i]);
+    options.appendChild(btn);
+  }
+}
+
+function evaluate(e) {
+  e.preventDefault();
+
+  let choice = e.target.textContent;
+
+  if (choice === questions[questionNum]) {
+  }
+  questionNum++;
+  setQuestion();
+}
+
+function gameOver() {
+  // Hides questions, shows endPage
+  quizzer.classList.toggle("hidden");
+  endPage.classList.toggle("hidden");
+}
+
+// Listen for click to select answer to question
+options.addEventListener("click", evaluate);
+
 // Listen for click event on Start Quiz button
-startBtn.addEventListener("click", takeQuiz());
+startBtn.addEventListener("click", setTime);
